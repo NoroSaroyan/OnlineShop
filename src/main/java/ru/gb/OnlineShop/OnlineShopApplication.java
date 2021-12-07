241532dc1754ce368dec6de4b538eaeba5b9502f
noriksaroyan <noriksaroyan@gmail.com>
2021-12-08 02:48:51 +0300

    Added encoding algorithm

--- src/main/java/ru/gb/OnlineShop/OnlineShopApplication.java
+++ src/main/java/ru/gb/OnlineShop/OnlineShopApplication.java
@@ -2,12 +2,27 @@ package ru.gb.OnlineShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import ru.gb.encoder.SHA256_impl;
+
+import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication
 public class OnlineShopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OnlineShopApplication.class, args);
-	}
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        SpringApplication.run(OnlineShopApplication.class, args);
+       /* String pswrd = "11111";
+        String hex1 = SHA256_impl.toHexString(SHA256_impl.getSHA(pswrd));
+        String newPassword = "11111";
+        String hex2 = SHA256_impl.toHexString(SHA256_impl.getSHA(newPassword));
+        if (hex1.equals(hex2)) {
+            System.out.println("accept");
+            System.out.println("Hash: " + hex1.toString());
+        } else {
+            System.out.println("not equal");
+            System.out.println("hex1: " + hex1);
+            System.out.println("hex2: " + hex2);
+        }*/
+    }
 
 }
--- src/main/java/ru/gb/service/UserService.java
+++ src/main/java/ru/gb/service/UserService.java
@@ -6,19 +6,24 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import ru.gb.encoder.SHA256_impl;
 import ru.gb.entity.User;
 import ru.gb.repository.RoleRepository;
 import ru.gb.repository.UserRepository;
+
+import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 
 @Service
 public class UserService implements UserDetailsService {
     private RoleRepository roleRepository;
     private UserRepository userRepository;
+    private SHA256_impl encoding;
 
     public UserService(RoleRepository roleRepository, UserRepository userRepository) {
         this.roleRepository = roleRepository;
         this.userRepository = userRepository;
+        this.encoding = new SHA256_impl();
     }
 
     @Override
@@ -36,6 +41,11 @@ public class UserService implements UserDetailsService {
                         .map(role -> new SimpleGrantedAuthority(role.getName()))
                         .toList()
         );
+    }
 
+    public void saveUser(User user) throws NoSuchAlgorithmException {
+        //if username isn't taken , then
+        String encodedPassword = SHA256_impl.toHexString(SHA256_impl.getSHA(user.getPassword()));
+        //user data to be sent to db ;
     }
 }
