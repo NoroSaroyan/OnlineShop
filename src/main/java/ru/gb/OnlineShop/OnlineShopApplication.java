d087b40d143418815065f8b73eb2c4562a659811
noriksaroyan <noriksaroyan@gmail.com>
2021-12-23 00:23:09 +0300

    fixes

--- src/main/java/ru/gb/OnlineShop/OnlineShopApplication.java
+++ src/main/java/ru/gb/OnlineShop/OnlineShopApplication.java
@@ -2,11 +2,12 @@ package ru.gb.OnlineShop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 //import ru.gb.encoder.SHA256_impl;
 
 import java.security.NoSuchAlgorithmException;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
 public class OnlineShopApplication {
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
